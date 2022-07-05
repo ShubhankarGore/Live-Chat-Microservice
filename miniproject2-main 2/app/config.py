0d34f0a8d04b902a3929d382370159315abadee7
ShubhankarGore <79012375+ShubhankarGore@users.noreply.github.com>
2022-07-05 13:24:31 -0400

    Delete miniproject2-main 2 directory

--- miniproject2-main 2/app/config.py
+++
@@ -1,3 +0,0 @@
-
-SECRET_KEY = 'xZxM5GMQ37CQw9kf6SRS33LadZTpSKt6'
-MONGODB_SETTINGS = {'db': 'mongodb', 'host': '0.0.0.0', 'port': 27017}  
--- miniproject2-main 2/run.py
+++
@@ -1,4 +0,0 @@
-from app import web_app, socketio
-
-if __name__ == '__main__':
-    socketio.run(web_app, host='0.0.0.0', port=8080)
